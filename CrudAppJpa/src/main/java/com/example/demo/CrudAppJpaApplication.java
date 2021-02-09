c779f4cfc7448ec82df05a640b1cd2c43a460125
ooo <dhiru0878@gmail.com>
2021-02-09 12:39:42 +0530

    Modified Start Class

--- CrudAppJpa/src/main/java/com/example/demo/CrudAppJpaApplication.java
+++ CrudAppJpa/src/main/java/com/example/demo/CrudAppJpaApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.service.UserService;
 
 @SpringBootApplication
 public class CrudAppJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudAppJpaApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(CrudAppJpaApplication.class, args);
+	
+		UserService userService = context.getBean(UserService.class);
+		System.out.println(userService);
 	}
 
 }
